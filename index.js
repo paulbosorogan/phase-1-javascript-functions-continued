9b50af10df8b411b1c64b1e94179ea26dd4b1cfd
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-23 17:29:21 -0400

    commit tree 8057c0f6712c1486a06b5f39a2c3c4f7215b8ee9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity === undefined){
+        return "This Saturday, I want to roller-skate!"
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`}
+}
+
+const mondayWork = function (something){
+    if (something === undefined){
+        return "This Monday, I will go to the office."
+    }
+    else { return `This Monday, I will ${something}.`}
+};
+mondayWork();
+
+function wrapAdjective(sign="*"){
+    return function (adjective="special"){
+           return `You are ${sign}${adjective}${sign}!`;
+        };
+    };
+wrapAdjective(sign)(adjective);
